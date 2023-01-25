48010207cdb80526af9b7d0ab1bc6f1d5abb3df0
kinyarasam <skinyara.30@gmail.com>
2023-01-25 11:25:48 +0300

    Updated Docs
    Updated the code to pass the Docstring requirement and follow the PEP8.
    
    Fixes #none.

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,16 +4,22 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """class BaseCaching a caching system that inherits from BaseCaching
+    """
+    BaseCache class that inherits from BaseCaching and is a caching system
     """
 
+    def __init__(self):
+        """
+        Initialize an empty cache_data dictionary and MAX_ITEMS
+        """
+        super().__init__()
+
     def put(self, key, item):
-        """Adds an item that allows storing and retrieving items
-        from a dictionary
+        """Put an item in the cache_data dictionary
 
         Args:
-            key:
-            item:
+            key (str): key to be used as an index
+            item: item to be stored
 
         Returns:
             nothing if the key and item is none.
@@ -23,12 +29,12 @@ class BasicCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieves an item by key.
+        """Get the item from cache_data
 
         Args:
-            key: __
+            key(str): key to get the item
 
         Returns:
-            ___
+            the item or None if the the key does not exist
         """
         return self.cache_data.get(key, None)
