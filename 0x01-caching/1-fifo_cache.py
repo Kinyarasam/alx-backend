1f76b2ae2791e6cb1b66319ab333fc6941fe3e08
kinyarasam <skinyara.30@gmail.com>
2023-01-25 19:53:12 +0300

    Implement FIFO caching system
    This commit implements a caching system that uses the First In First Out (FIFO) algorithm.
    The class FIFOCache inherits from BaseCaching, and it follows the pycodestyle and PEP8 guidelines.
    The put method discards the oldest item in the cache when the number of items in the cache exceeds the MAX_ITEMS attribute
    The get method remains the same as the parent calss but with the proper documentation.
    
    Fixes #none

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+"""
+import collections
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A caching system that uses the First In First Out (FIFO) algorithm
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_order = collections.deque()
+
+    def put(self, key: str, item: any) -> None:
+        """Add an item to the cache.
+        If the key or item is none, method should not do anything
+        If number of items in self.cache_data is higher than
+        BaseCaching.MAX_ITEMS, the oldest item in the cache is discarded.
+
+        Args:
+            key (str): The key to associate with the item.
+            item (any): The item to be added to the cache.
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            oldest_key = self.cache_order.popleft()
+            self.cache_data.pop(oldest_key)
+            print("DISCARD: {}".format(oldest_key))
+
+        self.cache_data[key] = item
+        self.cache_order.append(key)
+
+    def get(self, key: str) -> any:
+        """Retrieve an item from the cache.
+
+        Args:
+            key (str): The key associated with the item to be retrieved.
+
+        Returns:
+            any: The item associated with the key or None if the key is not
+                in the cache.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
