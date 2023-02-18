0001061a331c3733ac9ae25b1a0cae5c87479e2f
kinyarasam <skinyara.30@gmail.com>
2023-02-18 16:28:37 +0300

    [Fix]: LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,13 +13,14 @@ class LRUCache(BaseCaching):
         """Initialize the cache
         """
         super().__init__()
-        self.cache_order = collections.OrderedDict()
+        # self.cache_order = collections.OrderedDict()
+        self.cache_order = []
 
     def put(self, key: str, item: any) -> None:
         """Add an item to the cache.
         If the key or item is none, method should not do anything
         If number of items in self.cache_data is higher than
-        BaseCaching.MAX_ITEmS, the least recently used item in the cache is
+        BaseCaching.MAX_ITEMS, the least recently used item in the cache is
         discarded.
 
         Args:
@@ -28,14 +29,23 @@ class LRUCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        elif key in self.cache_order:
-            self.cache_order.move_to_end(key)
-        elif len(self.cache_order) >= self.MAX_ITEMS:
-            oldest_key = next(iter(self.cache_order))
-            self.cache_order.pop(oldest_key)
-            print("DISCARD: {}".format(oldest_key))
+        # elif key in self.cache_order:
+        #    self.cache_order.move_to_end(key)
+        # elif len(self.cache_order) >= self.MAX_ITEMS:
+        #    oldest_key = next(iter(self.cache_order))
+        #     self.cache_order.pop(oldest_key)
+        #     print("DISCARD: {}".format(oldest_key))
 
-        self.cache_order[key] = item
+        # self.cache_order[key] = item
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                and key not in self.cache_data:
+                    print("Discard: {}".format(self.cache_order[0]))
+                    self.cache_data.pop(self.cache_order[0])
+                    del self.cache_order[0]
+        elif key in self.cache_order:
+            del self.cache_order[self.cache_order.index(key)]
+        self.cache_order.append(key)
+        self.cache_data[key] = item
 
     def get(self, key: str) -> any:
         """Retrieves an item fro the cache.
@@ -50,5 +60,8 @@ class LRUCache(BaseCaching):
         if key is None or key not in self.cache_order:
             return None
 
-        self.cache_order.move_to_end(key)
-        return self.cache_order[key]
+        # self.cache_order.move_to_end(key)
+        # return self.cache_order[key]
+        del self.cache_order[self.cache_order.index(key)]
+        self.cache_order.append(key)
+        return self.cache_data[key]
