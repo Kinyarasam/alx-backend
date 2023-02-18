d6b3e4e1529a5c69745da5df2e4f5a8289384335
kinyarasam <skinyara.30@gmail.com>
2023-02-18 17:02:14 +0300

    LFU Caching
    Class LFUCache inherits from Basecaching and is a caching system
    Method put: assigns to the dictionary sel.cache_data the item value for the key key.
    Method get: Returns the value in self.cache_data linked to key.
    
    Fixes #none.

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+""" LFU Caching algorithm module
+"""
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """A caching system that uses the Least Frequentlu Used algorithm
+    """
+
+    def __init__(self):
+        """Initialize LFUCache
+        """
+        super().__init__()
+        self.history = []
+        self.frequency = {}
+
+    def put(self, key, item):
+        """
+        Cache a key-value pair
+
+        Args:
+            key (str): The key associated with the item
+            item (any): The item to be added to the cache
+        """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                lfu = min(self.frequency.values())
+                lfu_keys = []
+                for k, v in self.frequency.items():
+                    if v == lfu:
+                        lfu_keys.append(k)
+                if len(lfu_keys) > 1:
+                    lru_lfu = {}
+                    for k in lfu_keys:
+                        lru_lfu[k] = self.history.index(k)
+                    discard = min(lru_lfu.values())
+                    discard = self.history[discard]
+                else:
+                    discard = lfu_keys[0]
+
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+                del self.history[self.history.index(discard)]
+                del self.frequency[discard]
+            if key in self.frequency:
+                self.frequency[key] += 1
+            else:
+                self.frequency[key] = 1
+            if key in self.history:
+                del self.history[self.history.index(key)]
+            self.history.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve the value in self.cache_data linked to key
+
+        Args:
+            key (str): The key associate with the item to be retrieved
+
+        Returns:
+            None
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.history[self.history.index(key)]
+            self.history.append(key)
+            self.frequency[key] += 1
+            return self.cache_data[key]
+        return None
